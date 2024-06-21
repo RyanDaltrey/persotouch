3a1b5f1d37134663500ea668b76af6431361212c
Ryan <ryandaltrey@hotmail.com>
2024-06-21 16:02:23 +0200

    ADD app.use pour executer la requette getCategoriesByID

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const CategoriesRoutes = require ("./Routes/CategoriesRoutes");
 
 app.use(express.json());
 app.use("/accueil" , CategoriesRoutes);
+app.use("/:id", CategoriesRoutes);
 
 module.exports = app;
