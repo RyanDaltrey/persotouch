3d0fc2809095e7e5212d0db8f3a1b48633d9fb30
Ryan <ryandaltrey@hotmail.com>
2024-06-21 15:11:59 +0200

    ADD app.use pour executer la requete de categoriesRoutes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express();
+const CategoriesRoutes = require ("./Routes/CategoriesRoutes");
 
 app.use(express.json());
-
+app.use("/accueil" , CategoriesRoutes);
 module.exports = app;
