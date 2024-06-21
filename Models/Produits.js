be0c8a14b350c0355517d4fe37c851170d82a965
Ryan <ryandaltrey@hotmail.com>
2024-06-21 13:12:04 +0200

    ADD fichier Produits pour representer la table produits de notre bdd grace au class extends Model

---
+++ Models/Produits.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require ("../Config/Sequelize");
+
+class Produits extends Model {
+
+}
+
+Produits.init ({
+    id_pro : {
+        type : DataTypes.INTEGER,
+        autoIncrement : true
+    },
+    nom_pro : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    des_pro : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    prix : {
+        type : DataTypes.FLOAT,
+        allowNull : false
+    },
+    quantite_stock : {
+        type : DataTypes.INTEGER,
+        allowNull : false
+    },
+    img_cat : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    id_cat : {
+        type : DataTypes.INTEGER,
+        allowNull: false
+    };
+
+}, {
+    sequelize,
+    modelName : "Produits",
+    tableName : "produits",
+    timestamps : false,
+});
+
+    module.exports = Produits;
