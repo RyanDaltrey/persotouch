21e13e1a3128b2245373ed8674242fac4036f9bf
Ryan <ryandaltrey@hotmail.com>
2024-06-21 15:55:13 +0200

    ADD la fonctionalité getCategoriesByID

--- Services/CategoriesService.js
+++ Services/CategoriesService.js
@@ -5,6 +5,10 @@ class CategoriesService {
     async getAllCategories(){
         return await Categories.findAll();
     }
+
+    async getCategoriesByID(categoriesID){
+        return await Categories.findByPk(categoriesID)
+    }
 }
 
 module.exports = new CategoriesService();
