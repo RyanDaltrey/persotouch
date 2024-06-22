e4a6868a4445586b15db3258804e06a8b337aeb1
Ryan <ryandaltrey@hotmail.com>
2024-06-23 01:45:32 +0200

    ADD Routes pour updateClients.

--- Routes/ClientsRoutes.js
+++ Routes/ClientsRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get("/" , (req,res) => ClientsControllers.getAllClients(req,res));
 router.get("/:id" , (req,res) => ClientsControllers.getClientsByID(req,res));
 router.post("/" , (req,res) => ClientsControllers.addClients(req,res));
+router.patch("/:id", (req,res) => ClientsControllers.updateClients(req,res));
 
 module.exports = router;
