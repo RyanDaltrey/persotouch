46df3cd6788d2693320888ffe79d6c38f756b8e8
Ryan <ryandaltrey@hotmail.com>
2024-06-23 00:59:17 +0200

    ADD requette getClientsByID

--- Controllers/ClientsControllers.js
+++ Controllers/ClientsControllers.js
@@ -11,6 +11,16 @@ class ClientsControllers{
             result.json ({error : "Une erreur est survenue lors de la récupération des clients."})
         }
     }
+
+    async getClientsByID(request,result){
+        try {
+            const client = await Clients.getClientsByID(request.params.id)
+            result.json(client);
+        } catch (error) {
+            result.status(500)
+            result.json({error : "Une erreur est survenue lors de la récupération de clients par ID."})
+        }
+    }
 }
 
 module.exports = new ClientsControllers();
