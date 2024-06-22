a14530ad981ff0aae78a32ca422dfb86f497a414
Ryan <ryandaltrey@hotmail.com>
2024-06-23 01:00:42 +0200

    ADD Routes pour getClientsByID

--- Routes/ClientsRoutes.js
+++ Routes/ClientsRoutes.js
@@ -3,5 +3,6 @@ const express = require ("express");
 const router = express.Router();
 
 router.get("/" , (req,res) => ClientsControllers.getAllClients(req,res));
+router.get("/:id" , (req,res) => ClientsControllers.getClientsByID(req,res));
 
 module.exports = router;
