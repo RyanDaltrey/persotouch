fa2f1045cd0a8037a2197402ec823b10d06c2473
Ryan <ryandaltrey@hotmail.com>
2024-06-22 23:53:07 +0200

    ADD Routes pour deleteCategories.

--- Routes/CategoriesRoutes.js
+++ Routes/CategoriesRoutes.js
@@ -6,4 +6,5 @@ router.get("/", (req,res) => CategoriesController.getAllCategories(req,res));
 router.get("/:id", (req, res) => CategoriesController.getCategoriesByID(req,res));
 router.post("/", (req,res) => CategoriesController.addCategories(req,res));
 router.patch("/:id", (req,res) => CategoriesController.updateCategories(req,res));
+router.delete("/:id", (req,res) => CategoriesController.deleteCategories(req,res));
 module.exports = router; 
