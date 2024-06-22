d39d4385ad18febcf94bad86f5dae1ad71ffe112
Ryan <ryandaltrey@hotmail.com>
2024-06-23 00:40:49 +0200

    ADD app.use /clients pour executer la requete de ClientsRoutes.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 const app = express();
 const CategoriesRoutes = require ("./Routes/CategoriesRoutes");
+const ClientsRoutes = require ("./Routes/ClientsRoutes");
 
 app.use(express.json());
 app.use("/accueil" , CategoriesRoutes);
+app.use("/clients" , ClientsRoutes);
 
 
 module.exports = app;
