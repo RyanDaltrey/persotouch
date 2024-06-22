11843bcff17fa1e9f9926354b7e5e1293654d31f
Ryan <ryandaltrey@hotmail.com>
2024-06-23 01:59:01 +0200

    ADD Routes pour deleteClients.

--- Routes/ClientsRoutes.js
+++ Routes/ClientsRoutes.js
@@ -6,5 +6,6 @@ router.get("/" , (req,res) => ClientsControllers.getAllClients(req,res));
 router.get("/:id" , (req,res) => ClientsControllers.getClientsByID(req,res));
 router.post("/" , (req,res) => ClientsControllers.addClients(req,res));
 router.patch("/:id", (req,res) => ClientsControllers.updateClients(req,res));
+router.delete("/:id", (req,res) => ClientsControllers.deleteClients(req,res));
 
 module.exports = router;
