47c5fa45522121af061957878e628a03fb31fca9
Ryan <ryandaltrey@hotmail.com>
2024-06-22 23:42:17 +0200

    ADD routes pour UpdateCategories

--- Routes/CategoriesRoutes.js
+++ Routes/CategoriesRoutes.js
@@ -5,4 +5,5 @@ const router = express.Router();
 router.get("/", (req,res) => CategoriesController.getAllCategories(req,res));
 router.get("/:id", (req, res) => CategoriesController.getCategoriesByID(req,res));
 router.post("/", (req,res) => CategoriesController.addCategories(req,res));
+router.patch("/:id", (req,res) => CategoriesController.updateCategories(req,res));
 module.exports = router; 
