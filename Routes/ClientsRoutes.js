fb948cb65d68ae3c5f1be25913504aa7874318b7
Ryan <ryandaltrey@hotmail.com>
2024-06-23 01:18:21 +0200

    ADD Routes pour addClients.

--- Routes/ClientsRoutes.js
+++ Routes/ClientsRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/" , (req,res) => ClientsControllers.getAllClients(req,res));
 router.get("/:id" , (req,res) => ClientsControllers.getClientsByID(req,res));
+router.post("/" , (req,res) => ClientsControllers.addClients(req,res));
 
 module.exports = router;
