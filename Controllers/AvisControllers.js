a0f26243e4bed3becdcc639089338931f91342bd
Ryan <ryandaltrey@hotmail.com>
2024-06-23 04:08:28 +0200

    ADD fichier AvisControllers et ajout de la requette getAllAvis.

---
+++ Controllers/AvisControllers.js
@@ -0,0 +1,14 @@
+const AvisService = require ('../Services/AvisService');
+
+class AvisController{
+
+    async getAllAvis(request, result){
+        try {
+            const avis = await AvisService.getAllAvis()
+            result.json(avis);
+        } catch (error) {
+            result.status(500)
+            result.json({ error : "Une erreur est survenue lors de la récupération des avis."})
+        }
+    }
+}
