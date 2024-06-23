99f8960a58dc32ea6824058d89b837420d492b70
Ryan <ryandaltrey@hotmail.com>
2024-06-23 18:20:57 +0200

    ADD requette updatePaniers.

--- Controllers/PaniersControllers.js
+++ Controllers/PaniersControllers.js
@@ -31,5 +31,15 @@ class PaniersControllers {
             result.json({error : "Une erreur est survenue lors de l'ajout du panier."})
         }
     }
+
+    async updatePaniers(request, result){
+        try {
+            const paniers = await PaniersService.updatePaniers(request.params.id , params.body)
+            result.json(paniers);
+        } catch (error) {
+            result.status(500)
+            result.json({error : "Une erreur est survenue lors de la mise a jour du panier."})
+        }
+    }
 }
 module.exports = new PaniersControllers();
