327de85c7eda75d7aaba8177e86263dd51e8c442
Ryan <ryandaltrey@hotmail.com>
2024-06-23 14:41:14 +0200

    ADD fonctionalité getCommandesByID.

--- Services/CommandesService.js
+++ Services/CommandesService.js
@@ -5,5 +5,9 @@ class CommandesService {
     async getAllCommandes(){
         return await Commandes.findAll();
     }
+
+    async getCommandesByID(commandesID){
+        return await Commandes.findByPk(commandesID);
+    }
 }
 module.exports = new CommandesService();
