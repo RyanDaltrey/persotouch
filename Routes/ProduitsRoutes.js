546359cf89ea970b1cec566b8e96561fd64b1c7e
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:08:11 +0200

    ADD Routes pour getProduitsbyID.

--- Routes/ProduitsRoutes.js
+++ Routes/ProduitsRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ProduitsControllers = require ("../Controllers/ProduitsControllers");
 
 router.get("/", (req,res) => ProduitsControllers.getAllProduits(req,res));
+router.get("/:id", (req,res) => ProduitsControllers.getProduitsByID(req,res));
 
 module.exports = router;
