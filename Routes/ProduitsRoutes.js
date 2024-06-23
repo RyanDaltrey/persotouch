729ee89c53b12a5e0d336320542719b062207ba2
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:32:43 +0200

    ADD Routes pour deleteProduits.

--- Routes/ProduitsRoutes.js
+++ Routes/ProduitsRoutes.js
@@ -6,5 +6,5 @@ router.get("/", (req,res) => ProduitsControllers.getAllProduits(req,res));
 router.get("/:id", (req,res) => ProduitsControllers.getProduitsByID(req,res));
 router.post("/" , (req,res) => ProduitsControllers.addProduits(req,res));
 router.patch("/:id", (req,res) => ProduitsControllers.updateProduits(req,res));
-
+router.delete("/:id", (req,res) => ProduitsControllers.deleteProduits(req,res));
 module.exports = router;
