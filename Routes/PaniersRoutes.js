5fe20ef8e68566473c1829efacfc6bae58ecc373
Ryan <ryandaltrey@hotmail.com>
2024-06-23 18:09:43 +0200

    ADD Routes pour getPaniersByID.

--- Routes/PaniersRoutes.js
+++ Routes/PaniersRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const PaniersControllers = require("../Controllers/PaniersControllers");
 
 router.get("/", (req,res) => PaniersControllers.getAllPaniers(req,res));
+router.get("/:id", (req,res) => PaniersControllers.getPaniersByID(req,res));
+
 
 module.exports = router;
