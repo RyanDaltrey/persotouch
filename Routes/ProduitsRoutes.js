d4b0183a02d45f556324fd42a4321f5404465c28
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:25:16 +0200

    ADD Routes pour udateProduits.

--- Routes/ProduitsRoutes.js
+++ Routes/ProduitsRoutes.js
@@ -5,5 +5,6 @@ const ProduitsControllers = require ("../Controllers/ProduitsControllers");
 router.get("/", (req,res) => ProduitsControllers.getAllProduits(req,res));
 router.get("/:id", (req,res) => ProduitsControllers.getProduitsByID(req,res));
 router.post("/" , (req,res) => ProduitsControllers.addProduits(req,res));
+router.patch("/:id", (req,res) => ProduitsControllers.updateProduits(req,res));
 
 module.exports = router;
