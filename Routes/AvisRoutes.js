a84da62c6dc58ee5e04cbfaf10d724d125144186
Ryan <ryandaltrey@hotmail.com>
2024-06-23 04:56:48 +0200

    ADD Routes deleteAvis.

--- Routes/AvisRoutes.js
+++ Routes/AvisRoutes.js
@@ -6,4 +6,5 @@ router.get("/", (req,res) => AvisController.getAllAvis(req,res));
 router.get("/:id" , (req,res) => AvisController.getAvisByID(req,res));
 router.post("/", (req,res) => AvisController.addAvis(req,res));
 router.patch("/:id" , (req,res) => AvisController.updateAvis(req,res));
+router.delete("/:id", (req,res) => AvisController.deleteAvis(req,res));
 module.exports = router;
