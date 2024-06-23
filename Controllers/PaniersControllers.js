dad8c656eaf93721192d02e04fa95e8a70775990
Ryan <ryandaltrey@hotmail.com>
2024-06-23 18:08:31 +0200

    ADD requette getPaniersByID.

--- Controllers/PaniersControllers.js
+++ Controllers/PaniersControllers.js
@@ -11,5 +11,15 @@ class PaniersControllers {
             result.json({error : "Une erreur est survenue lors de la réception des paniers."})
         }
     }
+
+    async getPaniersByID(request, result){
+        try {
+            const panier = await PaniersService.getPaniersByID(request.params.id)
+            result.json(panier);
+        } catch (error) {
+            result.status(500)
+            result.json({error : "Une erreur est survenue lors de la récupération du panier par ID."})
+        }
+    }
 }
 module.exports = new PaniersControllers();
