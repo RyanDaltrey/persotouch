ecc3a27f596262b78089e8fc2e16db59230b67d3
Ryan <ryandaltrey@hotmail.com>
2024-06-23 14:45:02 +0200

    ADD Routes pour getCommandesByID.

--- Routes/CommandesRoutes.js
+++ Routes/CommandesRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CommandesControllers = require ("../Controllers/CommandesControllers");
 
 router.get("/", (req,res) => CommandesControllers.getAllControllers(req,res));
+router.get("/:id", (req,res) => CommandesControllers.getCommandesByID(req,res));
 
 module.exports = router;
