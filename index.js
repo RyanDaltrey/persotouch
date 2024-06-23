ae3bce2a7ffcbe17dc3fd55dd1b0e2dc0f9029a8
Ryan <ryandaltrey@hotmail.com>
2024-06-23 14:26:20 +0200

    ADD app.use /commandes pour executer les requette pour commandes.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const CategoriesRoutes = require ("./Routes/CategoriesRoutes");
 const ClientsRoutes = require ("./Routes/ClientsRoutes");
 const AvisRoutes = require ("./Routes/AvisRoutes");
+const CommandesRoutes = require ("./Routes/CommandesRoutes");
 
 
 app.use(express.json());
 app.use("/accueil" , CategoriesRoutes);
 app.use("/clients" , ClientsRoutes);
 app.use("/avis", AvisRoutes);
+app.use("/commandes", CommandesRoutes);
 
 
 module.exports = app;
