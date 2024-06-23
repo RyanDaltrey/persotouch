0d4d0f34ce61849cb1e43549f2a6eadc1cd86517
Ryan <ryandaltrey@hotmail.com>
2024-06-23 04:20:42 +0200

    ADD app.use /avis pour executer la requette getAllAvis.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require ('express');
 const app = express();
 const CategoriesRoutes = require ("./Routes/CategoriesRoutes");
 const ClientsRoutes = require ("./Routes/ClientsRoutes");
+const AvisRoutes = require ("./Routes/AvisRoutes");
+
 
 app.use(express.json());
 app.use("/accueil" , CategoriesRoutes);
 app.use("/clients" , ClientsRoutes);
+app.use("/avis", AvisRoutes);
 
 
 module.exports = app;
