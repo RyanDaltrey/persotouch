b6b5726e6762efffffca65fa77ad64da00b3731b
Ryan <ryandaltrey@hotmail.com>
2024-06-23 04:41:27 +0200

    ADD Routes pour addAvis.

--- Routes/AvisRoutes.js
+++ Routes/AvisRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/", (req,res) => AvisController.getAllAvis(req,res));
 router.get("/:id" , (req,res) => AvisController.getAvisByID(req,res));
+router.post("/", (req,res) => AvisController.addAvis(req,res));
 
 module.exports = router;
