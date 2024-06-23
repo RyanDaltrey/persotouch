fefe3d3b09c00af96ca80a8e59c17bdd3dc8f8f4
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:53:13 +0200

    ADD Routed pour getCommandeProduitsByID.

--- Routes/CommandeProduitsRoutes.js
+++ Routes/CommandeProduitsRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CommandeProduitsControllers = require ("../Controllers/CommandeProduitsControllers");
 
 router.get("/", (req,res) => CommandeProduitsControllers.getAllCommandeProduits(req,res));
+router.get("/:id" , (req,res) => CommandeProduitsControllers.getCommandeProduitsByID(req,res));
 
 module.exports = router;
