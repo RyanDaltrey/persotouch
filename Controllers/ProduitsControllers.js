b828c037878b3492b418a4e47770cd87ecb9a166
Ryan <ryandaltrey@hotmail.com>
2024-06-23 15:52:15 +0200

    ADD requettes getAllProduits.

---
+++ Controllers/ProduitsControllers.js
@@ -0,0 +1,14 @@
+const ProduitsServices = require ("../Services/ProduitsServices");
+
+class ProduitsControllers {
+
+    async getAllControllers(request, result){
+        try {
+            const produits = await ProduitsServices.getAllProduits()
+            result.json(produits);
+        } catch (error) {
+            result.status(500)
+            result.json({error : "Une erreur est survenue lors de la récupération des produits."})
+        }
+    }
+}
