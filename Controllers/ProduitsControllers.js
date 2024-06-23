231f4e0d46d441841c7039431930c54bd86b4fa1
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:05:09 +0200

    ADD requette getProduitsByID.

--- Controllers/ProduitsControllers.js
+++ Controllers/ProduitsControllers.js
@@ -11,5 +11,15 @@ class ProduitsControllers {
             result.json({error : "Une erreur est survenue lors de la récupération des produits."})
         }
     }
+
+    async getProduitsByID(request, result){
+        try {
+            const produit = await ProduitsServices.getProduitsByID(request.params.id)
+            result.json(produit);
+        } catch (error) {
+            result.status(500)
+            result.json({ error : "Une erreur est survenue lors de la récupération de commande par ID."})
+        }
+    }
 }
 module.exports = new ProduitsControllers();
