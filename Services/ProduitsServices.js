392e475349137d1896be32cb863adf37038d8ef1
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:11:30 +0200

    ADD fonctionalité addProduits.

--- Services/ProduitsServices.js
+++ Services/ProduitsServices.js
@@ -9,5 +9,9 @@ class ProduitsServices {
     async getProduitsByID(produitsID){
         return await Produits.findByPk(produitsID);
     }
+
+    async addProduits(produitdata){
+        return await Produits.update(produitdata);
+    }
 }
 module.exports = new ProduitsServices();
