d1fb28c1342e096096c1874cd7dc64e74ebc487e
Ryan <ryandaltrey@hotmail.com>
2024-06-23 16:15:18 +0200

    ADD Routes pour addProduits.

--- Routes/ProduitsRoutes.js
+++ Routes/ProduitsRoutes.js
@@ -4,5 +4,6 @@ const ProduitsControllers = require ("../Controllers/ProduitsControllers");
 
 router.get("/", (req,res) => ProduitsControllers.getAllProduits(req,res));
 router.get("/:id", (req,res) => ProduitsControllers.getProduitsByID(req,res));
+router.post("/" , (req,res) => ProduitsControllers.addProduits(req,res));
 
 module.exports = router;
