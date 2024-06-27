5cbc00559869b367515dd87acd96187aff45d1ff
Ryan <ryandaltrey@hotmail.com>
2024-06-28 00:03:37 +0200

    ADD routes pour login

--- Routes/AuthenticateRoutes.js
+++ Routes/AuthenticateRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const AutheticateController = require ("../Controllers/AuthenticateControllers");
 
 router.post("/register" , (req, res) => AutheticateController.register(req,res));
-
+router.post("/login", (req,res) => AutheticateController.login(req,res));
 module.exports = router;
