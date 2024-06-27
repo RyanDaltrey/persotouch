dc2ae1d0dbb41d40d09affe90558bd5ffa1f1907
Ryan <ryandaltrey@hotmail.com>
2024-06-27 23:02:10 +0200

    ADD fichier Routes et creation de la route pour register.

---
+++ Routes/AuthenticateRoutes.js
@@ -0,0 +1,5 @@
+const express = require ("express");
+const router = express.Router();
+const AutheticateController = require ("../Controllers/AuthenticateControllers");
+
+router.post("/register" , (req, res) => AutheticateController.register(req,res));
